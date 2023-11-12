d4b061c35701e530953b74eaf60d7bddda357510
AntonyCheng <1911261716@qq.com>
2023-11-12 22:56:27 +0800

    v0.0.1 预演版本

--- demo/jwt-demo/src/test/java/top/sharehome/jwtdemo/JwtDemoApplicationTests.java
+++ demo/jwt-demo/src/test/java/top/sharehome/jwtdemo/JwtDemoApplicationTests.java
@@ -1,8 +1,5 @@
 package top.sharehome.jwtdemo;
 
-import cn.hutool.core.util.CharsetUtil;
-import cn.hutool.crypto.SecureUtil;
-import cn.hutool.crypto.symmetric.AES;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -10,9 +7,6 @@ import top.sharehome.jwtdemo.entity.vo.LoginVo;
 import top.sharehome.jwtdemo.utils.JwtUtils;
 
 import javax.annotation.Resource;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootTest
 class JwtDemoApplicationTests {
@@ -24,7 +18,7 @@ class JwtDemoApplicationTests {
 
     @Test
     void testCreate() {
-        String token = jwtUtils.createLoginToken(new LoginVo(123L, "17380271985", 123L, 2));
+        String token = jwtUtils.createLoginToken(new LoginVo());
         System.out.println(token);
     }
 
